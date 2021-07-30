ce4e3a9ffc33b1514d036c5703584de78e12aa22
LswaN58 <lwswanson2@wisc.edu>
2021-07-30 06:57:57 -0500

    Add in a Dashboard api stub file.

---
+++ apis/DashboardAPI.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask_restful import Resource, Api, reqparse
+
+class DashboardAPI:
+    @staticmethod
+    def register(app:Flask):
+        api = Api(app)
+        api.add_resource(DashboardAPI.Player, '/player/<player_id>/metrics')
+
+    class Player(Resource):
+        def get(self, player_id):
+            parser = reqparse.RequestParser()
+            parser.add_argument("teacher_id")
+            parser.add_argument("metrics")
+            args = parser.parse_args()
+            return {
+								"metrics":{i:f"fake {i}" for i in args['metrics']},
+                "message":f"Found these fake metrics for fake player {player_id}, for fake teacher {args['teacher_id']}"
+            }
