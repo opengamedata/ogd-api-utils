5e146ff676e9b5206f0bc7b816831392760e77cd
LswaN58 <lwswanson2@wisc.edu>
2021-08-24 11:07:43 -0500

    Add a config folder to configurate configuration items on the server.

---
+++ config/config.py
@@ -0,0 +1,22 @@
+settings = {
+    "db_config":
+    {
+        "id_gen": {
+            "DB_HOST" : "127.0.0.1",
+            "DB_USER" : "ogd-api",
+            "DB_PW" : "ogdlocalpass",
+            "DB_NAME" : "id_generator",
+            "DB_PORT" : 3306
+        },
+        "fd_users": {
+            "DB_HOST" : "127.0.0.1",
+            "DB_USER" : "ogd-api",
+            "DB_PW" : "ogdlocalpass",
+            "DB_NAME" : "fieldday_users",
+            "DB_PORT" : 3306
+        }
+    },
+    "OGD_CORE_PATH":"..\opengamedata-core",
+    "GOOGLE_CLIENT_ID":"538048568897-ucp8nbgqgfr51vd3ulns8lgptkq2lsm0.apps.googleusercontent.com",
+    "DEBUG_LEVEL":"DEBUG"
+}
