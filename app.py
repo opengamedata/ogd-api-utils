5c7b68f3f33a767b6e145a96594d08ed34215e54
LswaN58 <lwswanson2@wisc.edu>
2021-09-17 16:20:56 -0500

    Add Hello API to app

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
-import sys
-sys.path.append('/var/www/wsgi-bin')
 from flask import Flask
 from apis.ClassroomAPI import ClassroomAPI
 from apis.DashboardAPI import DashboardAPI
 from apis.GameStateAPI import GameStateAPI
+from apis.HelloAPI import HelloAPI
 from apis.PlayerAPI import PlayerAPI
 
 application = Flask(__name__)
@@ -16,6 +15,7 @@ def home():
 ClassroomAPI.register(application)
 DashboardAPI.register(application)
 GameStateAPI.register(application)
+HelloAPI.register(application)
 PlayerAPI.register(application)
 
 # if __name__ == '__main__':
