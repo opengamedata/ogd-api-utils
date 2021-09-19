9870cf68fb203cb60198eb8e8a0516e6e94ecf27
LswaN58 <lwswanson2@wisc.edu>
2021-09-19 09:56:46 -0500

    Slightly nicer printouts to separate sections for TestDriver

--- TestDriver.py
+++ TestDriver.py
@@ -1,11 +1,11 @@
 from tests.t_HelloAPI import t_HelloAPI
 from tests.t_GameStateAPI import t_GameStateAPI
 
-test_Hello = t_HelloAPI()
-# print("Running test_Hello:")
+# test_Hello = t_HelloAPI()
+# print("***\nRunning test_Hello:")
 # test_Hello.RunAll()
-# print("Done")
+# print("Done\n***")
 test_GameState = t_GameStateAPI()
-print("Running test_GameState:")
+print("***\nRunning test_GameState:\n")
 test_GameState.RunAll()
-print("Done")
+print("Done\n***")
