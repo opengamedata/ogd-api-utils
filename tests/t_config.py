14666fa3454982ea3de6991135652d7f8843f09c
LswaN58 <lwswanson2@wisc.edu>
2021-09-19 18:48:47 -0500

    Add a bit to config to allow enabling/disabling different tests easily.

--- tests/t_config.py
+++ tests/t_config.py
@@ -1,3 +1,11 @@
 settings = {
-	"EXTERN_SERVER":"https://fieldday-web.wcer.wisc.edu/wsgi-bin/opengamedata.wsgi"
+    "EXTERN_SERVER":"https://fieldday-web.wcer.wisc.edu/wsgi-bin/opengamedata.wsgi"
+}
+
+EnabledTests = {
+    "CLASS":False,
+    "DASH":False,
+    "GAME_STATE":False,
+    "HELLO":True,
+    "PLAYER":True
 }
