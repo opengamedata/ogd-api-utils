5a7220c738c9f370d92346646d5f8bb864c71c6e
LswaN58 <lwswanson2@wisc.edu>
2021-11-08 08:41:31 -0600

    Enable DashboardAPI, and add back in the OGD_CORE_PATH import.

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
+import sys
 from flask import Flask
+# Local imports
+from config.config import settings
+if not settings['OGD_CORE_PATH'] in sys.path:
+    sys.path.append(settings['OGD_CORE_PATH'])
 from apis.ClassroomAPI import ClassroomAPI
-# from apis.DashboardAPI import DashboardAPI
+from apis.DashboardAPI import DashboardAPI
 from apis.GameStateAPI import GameStateAPI
 from apis.HelloAPI import HelloAPI
 from apis.PlayerAPI import PlayerAPI
@@ -13,7 +18,7 @@ def home():
     return "Home"
     
 ClassroomAPI.register(application)
-# DashboardAPI.register(application)
+DashboardAPI.register(application)
 GameStateAPI.register(application)
 HelloAPI.register(application)
 PlayerAPI.register(application)
