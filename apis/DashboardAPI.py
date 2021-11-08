bbaf2d3bcd6081d0b9e0c5522031ab943dcf29e6
LswaN58 <lwswanson2@wisc.edu>
2021-11-08 08:44:13 -0600

    Improve robustness of Population request.

--- apis/DashboardAPI.py
+++ apis/DashboardAPI.py
@@ -30,12 +30,13 @@ class DashboardAPI:
             parser.add_argument("metrics")
             args = parser.parse_args()
             return {
-								"metrics":{i:f"fake {i}" for i in args['metrics']},
+                "metrics":{i:f"fake {i}" for i in args['metrics']},
                 "message":f"Found these fake metrics for fake player {player_id}, for fake teacher {args['teacher_id']}"
             }
     
     class Population(Resource):
         def get(self, game_id):
+            print("Received population request.")
             ret_val : Dict[str,Any] = {
                 "type":"GET",
                 "val":None,
@@ -44,37 +45,44 @@ class DashboardAPI:
             }
 
             parser = reqparse.RequestParser()
-            parser.add_argument("start_date")
-            parser.add_argument("end_date")
-            parser.add_argument("metrics")
-            args = parser.parse_args()
-            # set up interface and request
-            interface = MySQLInterface("WAVES", settings=settings)
-            # interface = BigQueryInterface(game_id=game_id, settings=settings)
-            table_schema = TableSchema(schema_name="FIELDDAY_MYSQL", schema_path=Path("../schemas/TABLES"))
-            _yesterday = datetime.now()-timedelta(days=1)
-            _range = ExporterRange.FromDateRange(date_min=_yesterday, date_max=datetime.now(), source=interface)
-            request = Request(interface=interface, range=_range, exporter_files=ExporterFiles())
-            # set up population processor
-            game_schema = GameSchema(schema_name="WAVES", schema_path=Path("../games/WAVES"))
-            pop_processor = PopulationProcessor(ExtractorClass=WaveExtractor, game_schema=game_schema)
-            # retrieve and process the data
-            _dummy = request.RetrieveSessionIDs()
-            sess_ids = _dummy if _dummy is not None else []
-            _dummy = request._interface.RowsFromIDs(sess_ids)
-            rows = _dummy if _dummy is not None else []
+            parser.add_argument("start_datetime", type=datetime, help="Invalid starting date, defaulting to 1 hour ago.")
+            parser.add_argument("end_datetime", type=datetime, help="Invalid ending date, defaulting to present time.")
+            parser.add_argument("metrics", help="Got bad list of metric, defaulting to all.")
+            args : Dict[str, Any] = parser.parse_args()
+            _start_time = args.get('start_datetime') or datetime.now()-timedelta(hours=1)
+            _end_time   = args.get('end_datetime')   or datetime.now()
+            print("Parsed args.")
             try:
-                for row in rows:
-                    evt = table_schema.RowToEvent(row)
-                    if evt.session_id in sess_ids:
-                        pop_processor.ProcessEvent(evt)
-                    else:
-                        print(f"Found a session ({evt.session_id}) which was not in the list of sessions for processing.")
+                os.chdir("var/www/opengamedata/")
+                # set up interface and request
+                interface = MySQLInterface(game_id, settings=settings)
+                print("Set up interface.")
+                # interface = BigQueryInterface(game_id=game_id, settings=settings)
+                # _range = ExporterRange.FromDateRange(date_min=_yesterday, date_max=datetime.now(), source=interface)
+                _range = ExporterRange.FromDateRange(date_min=_start_time, date_max=_end_time, source=interface)
+                _exp_types = ExporterTypes(events=False, sessions=False, population=True)
+                _exp_locs = ExporterLocations(files=False, dict=True)
+                print("Set up request params.")
+                request = Request(interface=interface, range=_range, exporter_types=_exp_types, exporter_locs=_exp_locs)
+                print("Set up request object.")
+                # retrieve and process the data
+                export_mgr = ExportManager(settings=settings)
+                print("Set up export manager object.")
+                result = export_mgr.ExecuteRequest(request=request, game_id=game_id)
+                print("Ran export request.")
+                os.chdir("../../../../")
             except Exception as err:
                 ret_val['msg'] = f"ERROR: Unknown error while processing data"
+                ret_val['status'] = "ERR_SRV"
+                print(f"Got exception: {str(err)}")
+                print(traceback.format_exc())
             else:
-                cols = pop_processor.GetPopulationFeatureNames()
-                vals = pop_processor.GetPopulationFeatures()
+                cols = []
+                vals = []
+                if 'population' in result:
+                    cols = result['population']['cols']
+                    vals = result['population']['vals']
+                ct = min(len(cols), len(vals))
                 ret_val['msg'] = "SUCCESS: Generated population features"
-                ret_val["val"] = {cols[i] : vals[i] for i in range(min(len(cols), len(vals)))}
+                ret_val["val"] = {cols[i] : vals[i] for i in range(ct)}
             return ret_val
