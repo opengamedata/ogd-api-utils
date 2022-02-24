8b306f33986d7add303ec1dc7ab707704fe81247
LswaN58 <lwswanson2@wisc.edu>
2022-02-24 08:50:34 -0600

    Set up DashboardAPI to use the return value convenience class, and wrap up adding SessionList request.

--- apis/DashboardAPI.py
+++ apis/DashboardAPI.py
@@ -13,6 +13,7 @@ from pathlib import Path
 from typing import Any, Dict, List, Tuple, Union
 # Local imports
 from config.config import settings
+from apis.APIResult import APIResult, RESTType, ResultStatus
 from opengamedata.interfaces.MySQLInterface import MySQLInterface
 from opengamedata.interfaces.BigQueryInterface import BigQueryInterface
 from opengamedata.managers.ExportManager import ExportManager
@@ -23,6 +24,7 @@ class DashboardAPI:
     def register(app:Flask):
         api = Api(app)
         api.add_resource(DashboardAPI.Population, '/game/<game_id>/metrics')
+        api.add_resource(DashboardAPI.SessionList, '/game/<game_id>/sessions/')
         api.add_resource(DashboardAPI.Sessions, '/game/<game_id>/sessions/metrics')
         api.add_resource(DashboardAPI.Session, '/game/<game_id>/session/<session_id>/metrics')
     
@@ -55,12 +57,7 @@ class DashboardAPI:
     class Population(Resource):
         def get(self, game_id):
             print("Received population request.")
-            ret_val : Dict[str,Any] = {
-                "type":"GET",
-                "val":None,
-                "msg":"",
-                "status":"SUCCESS",
-            }
+            ret_val = APIResult.Default(req_type=RESTType.GET)
             _end_time   : datetime = datetime.now()
             _start_time : datetime = _end_time-timedelta(hours=1)
 
@@ -88,33 +85,60 @@ class DashboardAPI:
                     result = export_mgr.ExecuteRequest(request=request, game_id=game_id, feature_overrides=_metrics)
                 os.chdir("../../../../")
             except Exception as err:
-                ret_val['msg'] = f"ERROR: Unknown error while processing data"
-                ret_val['status'] = "ERR_SRV"
+                ret_val.ServerErrored(f"ERROR: Unknown error while processing data")
                 print(f"Got exception: {str(err)}")
                 print(traceback.format_exc())
             else:
-                cols = []
-                vals = []
                 if result.get('population') is not None:
                     cols = [str(item) for item in result['population']['cols']]
                     vals = [str(item) for item in result['population']['vals']]
                     ct = min(len(cols), len(vals))
-                    ret_val['msg'] = "SUCCESS: Generated population features"
-                    ret_val["val"] = {cols[i] : vals[i] for i in range(ct)}
+                    ret_val.RequestSucceeded(
+                        msg="SUCCESS: Generated population features",
+                        val={cols[i] : vals[i] for i in range(ct)}
+                    )
                 else:
-                    ret_val['msg'] = "FAIL: No valid population features"
-                    ret_val['status'] = "ERR_REQ"
-            return ret_val
+                    ret_val.RequestErrored("FAIL: No valid population features")
+            return ret_val.ToDict()
+
+    class SessionList(Resource):
+        def get(self, game_id):
+            print("Received session list request.")
+            ret_val = APIResult.Default(req_type=RESTType.GET)
+
+            _end_time   : datetime = datetime.now()
+            _start_time : datetime = _end_time-timedelta(hours=1)
+
+            parser = reqparse.RequestParser()
+            parser.add_argument("start_datetime", type=datetime_from_iso8601, required=False, default=_start_time, nullable=True, help="Invalid starting date, defaulting to 1 hour ago.")
+            parser.add_argument("end_datetime",   type=datetime_from_iso8601, required=False, default=_end_time,   nullable=True, help="Invalid ending date, defaulting to present time.")
+            args = parser.parse_args()
+
+            _end_time   = args.get('end_datetime')   or _end_time
+            _start_time = args.get('start_datetime') or _start_time
+            try:
+                result = {}
+                os.chdir("var/www/opengamedata/")
+                interface = DashboardAPI.gen_interface(game_id=game_id)
+                if interface is not None:
+                    _range = ExporterRange.FromDateRange(date_min=_start_time, date_max=_end_time, source=interface)
+                    result["ids"] = _range.GetIDs()
+                os.chdir("../../../../")
+            except Exception as err:
+                ret_val.ServerErrored(f"ERROR: Unknown error while processing data")
+                print(f"Got exception: {str(err)}")
+                print(traceback.format_exc())
+            else:
+                if result.get('ids') is not None:
+                    ret_val.RequestSucceeded(msg="SUCCESS: Got ID list for given date range", val=result['ids'])
+                else:
+                    ret_val.RequestErrored("FAIL: Did not find IDs in the given date range")
+            return ret_val.ToDict()
 
     class Sessions(Resource):
         def get(self, game_id):
             print("Received session request.")
-            ret_val : Dict[str,Any] = {
-                "type":"GET",
-                "val":None,
-                "msg":"",
-                "status":"SUCCESS",
-            }
+            ret_val = APIResult.Default(req_type=RESTType.GET)
 
             parser = reqparse.RequestParser()
             parser.add_argument("session_ids")
@@ -129,7 +153,7 @@ class DashboardAPI:
                 interface = DashboardAPI.gen_interface(game_id=game_id)
                 if _metrics is not None and _session_ids is not None and interface is not None:
                     _range = ExporterRange.FromIDs(ids=_session_ids, source=interface)
-                    _exp_types = ExporterTypes(events=False, sessions=False, population=True)
+                    _exp_types = ExporterTypes(events=False, sessions=True, population=False)
                     _exp_locs = ExporterLocations(files=False, dict=True)
                     request = Request(interface=interface, range=_range, exporter_types=_exp_types, exporter_locs=_exp_locs)
                     # retrieve and process the data
@@ -137,33 +161,25 @@ class DashboardAPI:
                     result = export_mgr.ExecuteRequest(request=request, game_id=game_id, feature_overrides=_metrics)
                 os.chdir("../../../../")
             except Exception as err:
-                ret_val['msg'] = f"ERROR: Unknown error while processing data"
-                ret_val['status'] = "ERR_SRV"
+                ret_val.ServerErrored(f"ERROR: Unknown error while processing data")
                 print(f"Got exception: {str(err)}")
                 print(traceback.format_exc())
             else:
-                cols = []
-                vals = []
-                if result.get('population') is not None:
+                if result.get('sessions') is not None:
                     cols = [str(item) for item in result['population']['cols']]
                     vals = [str(item) for item in result['population']['vals']]
                     ct = min(len(cols), len(vals))
-                    ret_val['msg'] = "SUCCESS: Generated population features"
-                    ret_val["val"] = {cols[i] : vals[i] for i in range(ct)}
+                    ret_val.RequestSucceeded(
+                        msg="SUCCESS: Generated population features",
+                        val={cols[i] : vals[i] for i in range(ct)}
+                    )
                 else:
-                    ret_val['msg'] = "FAIL: No valid population features"
-                    ret_val['status'] = "ERR_REQ"
-            return ret_val
+                    ret_val.RequestErrored("FAIL: No valid population features")
+            return ret_val.ToDict()
     
     class Session(Resource):
         def get(self, game_id, session_id):
-            print("Received session request.")
-            ret_val : Dict[str,Any] = {
-                "type":"GET",
-                "val":None,
-                "msg":"",
-                "status":"SUCCESS",
-            }
+            ret_val = APIResult.Default(req_type=RESTType.GET)
 
             parser = reqparse.RequestParser()
             parser.add_argument("metrics")
@@ -176,7 +192,7 @@ class DashboardAPI:
                 interface = DashboardAPI.gen_interface(game_id=game_id)
                 if _metrics is not None and interface is not None:
                     _range = ExporterRange.FromIDs(ids=[session_id], source=interface)
-                    _exp_types = ExporterTypes(events=False, sessions=False, population=True)
+                    _exp_types = ExporterTypes(events=False, sessions=True, population=False)
                     _exp_locs = ExporterLocations(files=False, dict=True)
                     request = Request(interface=interface, range=_range, exporter_types=_exp_types, exporter_locs=_exp_locs)
                     # retrieve and process the data
@@ -184,21 +200,19 @@ class DashboardAPI:
                     result = export_mgr.ExecuteRequest(request=request, game_id=game_id, feature_overrides=_metrics)
                 os.chdir("../../../../")
             except Exception as err:
-                ret_val['msg'] = f"ERROR: Unknown error while processing data"
-                ret_val['status'] = "ERR_SRV"
+                ret_val.ServerErrored(f"ERROR: Unknown error while processing data")
                 print(f"Got exception: {str(err)}")
                 print(traceback.format_exc())
             else:
-                cols = []
-                vals = []
-                if result.get('population') is not None:
+                if result.get('sessions') is not None:
                     cols = [str(item) for item in result['population']['cols']]
                     vals = [str(item) for item in result['population']['vals']]
                     ct = min(len(cols), len(vals))
-                    ret_val['msg'] = "SUCCESS: Generated population features"
-                    ret_val["val"] = {cols[i] : vals[i] for i in range(ct)}
+                    ret_val.RequestSucceeded(
+                        msg="SUCCESS: Generated population features",
+                        val={cols[i] : vals[i] for i in range(ct)}
+                    )
                 else:
-                    ret_val['msg'] = "FAIL: No valid population features"
-                    ret_val['status'] = "ERR_REQ"
-            return ret_val
+                    ret_val.RequestErrored("FAIL: No valid population features")
+            return ret_val.ToDict()
     
