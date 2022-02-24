0a580b430a8a5e5245d84758eedef4608f779927
LswaN58 <lwswanson2@wisc.edu>
2022-02-23 23:14:18 -0600

    Finish setting up calls for specific sessions for Dashboard.

--- apis/DashboardAPI.py
+++ apis/DashboardAPI.py
@@ -22,11 +22,12 @@ class DashboardAPI:
     @staticmethod
     def register(app:Flask):
         api = Api(app)
-        api.add_resource(DashboardAPI.Player, '/player/<player_id>/metrics')
         api.add_resource(DashboardAPI.Population, '/game/<game_id>/metrics')
+        api.add_resource(DashboardAPI.Sessions, '/game/<game_id>/sessions/metrics')
+        api.add_resource(DashboardAPI.Session, '/game/<game_id>/session/<session_id>/metrics')
     
     @staticmethod
-    def _parse_list(list_str:str):
+    def parse_list(list_str:str) -> Union[List[Any], None]:
         ret_val = None
         if ("[" in list_str) and ("]" in list_str):
             start = list_str.index("[")
@@ -34,27 +35,23 @@ class DashboardAPI:
             ret_val = list_str[start+1:end].split(",")
         return ret_val
 
-    class Player(Resource):
-        def get(self, player_id):
-            print("Received player request.")
-            ret_val : Dict[str,Any] = {
-                "type":"GET",
-                "val":None,
-                "msg":"",
-                "status":"SUCCESS",
-            }
-            _end_time   : datetime = datetime.now()
-            _start_time : datetime = _end_time-timedelta(hours=1)
+    @staticmethod
+    def gen_interface(game_id):
+        ret_val = None
+        src_map = settings['GAME_SOURCE_MAP'].get(game_id)
+        if src_map is not None:
+            # set up interface and request
+            if src_map['interface'] == "MySQL":
+                ret_val = MySQLInterface(game_id, settings=settings)
+                print(f"Using MySQLInterface for {game_id}")
+            elif src_map['interface'] == "BigQuery":
+                ret_val = BigQueryInterface(game_id=game_id, settings=settings)
+                print(f"Using BigQueryInterface for {game_id}")
+            else:
+                ret_val = MySQLInterface(game_id, settings=settings)
+                print(f"Could not find a valid interface for {game_id}, defaulting to MySQL!")
+        return ret_val
 
-            parser = reqparse.RequestParser()
-            parser.add_argument("teacher_id")
-            parser.add_argument("metrics")
-            args = parser.parse_args()
-            return {
-                "metrics":{i:f"fake {i}" for i in args['metrics']},
-                "message":f"Found these fake metrics for fake player {player_id}, for fake teacher {args['teacher_id']}"
-            }
-    
     class Population(Resource):
         def get(self, game_id):
             print("Received population request.")
@@ -72,25 +69,16 @@ class DashboardAPI:
             parser.add_argument("end_datetime",   type=datetime_from_iso8601, required=False, default=_end_time,   nullable=True, help="Invalid ending date, defaulting to present time.")
             parser.add_argument("metrics",        type=str,                   required=False, default="[]",        nullable=True, help="Got bad list of metric, defaulting to all.")
             args : Dict[str, Any] = parser.parse_args()
-            _start_time = args.get('start_datetime') or _start_time
+
             _end_time   = args.get('end_datetime')   or _end_time
-            _metrics    = DashboardAPI._parse_list(args.get('metrics') or "")
+            _start_time = args.get('start_datetime') or _start_time
+            _metrics    = DashboardAPI.parse_list(args.get('metrics') or "")
+
             try:
                 result = {}
-                src_map = settings['GAME_SOURCE_MAP'].get(game_id)
-                if _metrics is not None and src_map is not None:
-                    os.chdir("var/www/opengamedata/")
-                    # set up interface and request
-                    if src_map['interface'] == "MySQL":
-                        interface = MySQLInterface(game_id, settings=settings)
-                        print(f"Using MySQLInterface for {game_id}")
-                    elif src_map['interface'] == "BigQuery":
-                        interface = BigQueryInterface(game_id=game_id, settings=settings)
-                        print(f"Using BigQueryInterface for {game_id}")
-                    else:
-                        interface = MySQLInterface(game_id, settings=settings)
-                        print(f"Could not find a valid interface for {game_id}, defaulting to MySQL!")
-                    # _range = ExporterRange.FromDateRange(date_min=_yesterday, date_max=datetime.now(), source=interface)
+                os.chdir("var/www/opengamedata/")
+                interface = DashboardAPI.gen_interface(game_id=game_id)
+                if _metrics is not None and interface is not None:
                     _range = ExporterRange.FromDateRange(date_min=_start_time, date_max=_end_time, source=interface)
                     _exp_types = ExporterTypes(events=False, sessions=False, population=True)
                     _exp_locs = ExporterLocations(files=False, dict=True)
@@ -98,7 +86,7 @@ class DashboardAPI:
                     # retrieve and process the data
                     export_mgr = ExportManager(settings=settings)
                     result = export_mgr.ExecuteRequest(request=request, game_id=game_id, feature_overrides=_metrics)
-                    os.chdir("../../../../")
+                os.chdir("../../../../")
             except Exception as err:
                 ret_val['msg'] = f"ERROR: Unknown error while processing data"
                 ret_val['status'] = "ERR_SRV"
@@ -117,3 +105,100 @@ class DashboardAPI:
                     ret_val['msg'] = "FAIL: No valid population features"
                     ret_val['status'] = "ERR_REQ"
             return ret_val
+
+    class Sessions(Resource):
+        def get(self, game_id):
+            print("Received session request.")
+            ret_val : Dict[str,Any] = {
+                "type":"GET",
+                "val":None,
+                "msg":"",
+                "status":"SUCCESS",
+            }
+
+            parser = reqparse.RequestParser()
+            parser.add_argument("session_ids")
+            parser.add_argument("metrics")
+            args = parser.parse_args()
+
+            _metrics     = DashboardAPI.parse_list(args.get('metrics') or "")
+            _session_ids = DashboardAPI.parse_list(args.get('session_ids') or "[]")
+            try:
+                result = {}
+                os.chdir("var/www/opengamedata/")
+                interface = DashboardAPI.gen_interface(game_id=game_id)
+                if _metrics is not None and _session_ids is not None and interface is not None:
+                    _range = ExporterRange.FromIDs(ids=_session_ids, source=interface)
+                    _exp_types = ExporterTypes(events=False, sessions=False, population=True)
+                    _exp_locs = ExporterLocations(files=False, dict=True)
+                    request = Request(interface=interface, range=_range, exporter_types=_exp_types, exporter_locs=_exp_locs)
+                    # retrieve and process the data
+                    export_mgr = ExportManager(settings=settings)
+                    result = export_mgr.ExecuteRequest(request=request, game_id=game_id, feature_overrides=_metrics)
+                os.chdir("../../../../")
+            except Exception as err:
+                ret_val['msg'] = f"ERROR: Unknown error while processing data"
+                ret_val['status'] = "ERR_SRV"
+                print(f"Got exception: {str(err)}")
+                print(traceback.format_exc())
+            else:
+                cols = []
+                vals = []
+                if result.get('population') is not None:
+                    cols = [str(item) for item in result['population']['cols']]
+                    vals = [str(item) for item in result['population']['vals']]
+                    ct = min(len(cols), len(vals))
+                    ret_val['msg'] = "SUCCESS: Generated population features"
+                    ret_val["val"] = {cols[i] : vals[i] for i in range(ct)}
+                else:
+                    ret_val['msg'] = "FAIL: No valid population features"
+                    ret_val['status'] = "ERR_REQ"
+            return ret_val
+    
+    class Session(Resource):
+        def get(self, game_id, session_id):
+            print("Received session request.")
+            ret_val : Dict[str,Any] = {
+                "type":"GET",
+                "val":None,
+                "msg":"",
+                "status":"SUCCESS",
+            }
+
+            parser = reqparse.RequestParser()
+            parser.add_argument("metrics")
+            args = parser.parse_args()
+
+            _metrics    = DashboardAPI.parse_list(args.get('metrics') or "")
+            try:
+                result = {}
+                os.chdir("var/www/opengamedata/")
+                interface = DashboardAPI.gen_interface(game_id=game_id)
+                if _metrics is not None and interface is not None:
+                    _range = ExporterRange.FromIDs(ids=[session_id], source=interface)
+                    _exp_types = ExporterTypes(events=False, sessions=False, population=True)
+                    _exp_locs = ExporterLocations(files=False, dict=True)
+                    request = Request(interface=interface, range=_range, exporter_types=_exp_types, exporter_locs=_exp_locs)
+                    # retrieve and process the data
+                    export_mgr = ExportManager(settings=settings)
+                    result = export_mgr.ExecuteRequest(request=request, game_id=game_id, feature_overrides=_metrics)
+                os.chdir("../../../../")
+            except Exception as err:
+                ret_val['msg'] = f"ERROR: Unknown error while processing data"
+                ret_val['status'] = "ERR_SRV"
+                print(f"Got exception: {str(err)}")
+                print(traceback.format_exc())
+            else:
+                cols = []
+                vals = []
+                if result.get('population') is not None:
+                    cols = [str(item) for item in result['population']['cols']]
+                    vals = [str(item) for item in result['population']['vals']]
+                    ct = min(len(cols), len(vals))
+                    ret_val['msg'] = "SUCCESS: Generated population features"
+                    ret_val["val"] = {cols[i] : vals[i] for i in range(ct)}
+                else:
+                    ret_val['msg'] = "FAIL: No valid population features"
+                    ret_val['status'] = "ERR_REQ"
+            return ret_val
+    
