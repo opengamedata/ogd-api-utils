bc45727f75af622f2fb2106333be99a4987cce1d
Luke Swanson <lwswanson2@wisc.edu>
2022-03-15 12:57:04 -0500

    Attempt to stop imports from killing the rest of the API.

--- app.py
+++ app.py
@@ -4,24 +4,54 @@ from flask import Flask
 from config.config import settings
 if not settings['OGD_CORE_PATH'] in sys.path:
     sys.path.append(settings['OGD_CORE_PATH'])
-from apis.ClassroomAPI import ClassroomAPI
-from apis.DashboardAPI import DashboardAPI
-from apis.GameStateAPI import GameStateAPI
-from apis.HelloAPI import HelloAPI
-from apis.PlayerAPI import PlayerAPI
 
 application = Flask(__name__)
 application.secret_key = b'thisisafakesecretkey'
 
+try:
+    from apis.ClassroomAPI import ClassroomAPI
+except ImportError as err:
+    print("Could not import Classroom API", sys.stderr)
+else:
+    ClassroomAPI.register(application)
+
+try:
+    from apis.DashboardAPI import DashboardAPI
+except ImportError as err:
+    print("Could not import Dashboard API", sys.stderr)
+else:
+    DashboardAPI.register(application)
+
+try:
+    from apis.GameStateAPI import GameStateAPI
+except ImportError as err:
+    print("Could not import GameStateAPI API", sys.stderr)
+else:
+    GameStateAPI.register(application)
+
+try:
+    from apis.HelloAPI import HelloAPI
+except ImportError as err:
+    print("Could not import Hello API", sys.stderr)
+else:
+    HelloAPI.register(application)
+
+try:
+    from apis.PlayerAPI import PlayerAPI
+except ImportError as err:
+    print("Could not import Player API", sys.stderr)
+else:
+    PlayerAPI.register(application)
+
 @application.route("/")
-def home():
+def home() -> str:
+    """Defines a message when pinging the root of the API
+
+    :return: The message for pinging API root.
+    :rtype: str
+    """
     return "Home"
     
-ClassroomAPI.register(application)
-DashboardAPI.register(application)
-GameStateAPI.register(application)
-HelloAPI.register(application)
-PlayerAPI.register(application)
 
 # if __name__ == '__main__':
 # 	application.run(debug=True)
