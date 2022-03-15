58d99d7764c542c6772f241e794fa329e822d15e
Luke Swanson <lwswanson2@wisc.edu>
2022-03-15 13:48:42 -0500

    Convert other printouts to use flask app logger.

--- app.py
+++ app.py
@@ -13,35 +13,35 @@ application.secret_key = b'thisisafakesecretkey'
 try:
     from apis.ClassroomAPI import ClassroomAPI
 except ImportError as err:
-    print("Could not import Classroom API", sys.stderr)
+    application.logger.warning("Could not import Classroom API", sys.stderr)
 else:
     ClassroomAPI.register(application)
 
 try:
     from apis.DashboardAPI import DashboardAPI
 except ImportError as err:
-    print("Could not import Dashboard API", sys.stderr)
+    application.logger.warning("Could not import Dashboard API", sys.stderr)
 else:
     DashboardAPI.register(application)
 
 try:
     from apis.GameStateAPI import GameStateAPI
 except ImportError as err:
-    print("Could not import GameStateAPI API", sys.stderr)
+    application.logger.warning("Could not import GameStateAPI API", sys.stderr)
 else:
     GameStateAPI.register(application)
 
 try:
     from apis.HelloAPI import HelloAPI
 except ImportError as err:
-    print("Could not import Hello API", sys.stderr)
+    application.logger.warning("Could not import Hello API", sys.stderr)
 else:
     HelloAPI.register(application)
 
 try:
     from apis.PlayerAPI import PlayerAPI
 except ImportError as err:
-    print("Could not import Player API", sys.stderr)
+    application.logger.warning("Could not import Player API", sys.stderr)
 else:
     PlayerAPI.register(application)
 
