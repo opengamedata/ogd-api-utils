1e08fc654c0bd2c9cbf04a6a7cc0b938c667599d
Luke Swanson <lwswanson2@wisc.edu>
2022-03-15 13:10:12 -0500

    Add basic copy-paste for player metrics, and fix some calls.

--- apis/DashboardAPI.py
+++ apis/DashboardAPI.py
@@ -18,6 +18,7 @@ from opengamedata.interfaces.MySQLInterface import MySQLInterface
 from opengamedata.interfaces.BigQueryInterface import BigQueryInterface
 from opengamedata.managers.ExportManager import ExportManager
 from opengamedata.managers.Request import Request, ExporterRange, ExporterTypes, ExporterLocations
+from opengamedata.schemas.GameSchema import GameSchema
 
 class DashboardAPI:
     """Class to define an API for the developer/designer dashboard"""
@@ -29,7 +30,11 @@ class DashboardAPI:
         :type app: Flask
         """
         api = Api(app)
+        api.add_resource(DashboardAPI.FeatureList, '/game/<game_id>/metrics/list')
         api.add_resource(DashboardAPI.Population, '/game/<game_id>/metrics')
+        api.add_resource(DashboardAPI.SessionList, '/game/<game_id>/players/')
+        api.add_resource(DashboardAPI.Sessions, '/game/<game_id>/players/metrics')
+        api.add_resource(DashboardAPI.Session, '/game/<game_id>/player/<player_id>/metrics')
         api.add_resource(DashboardAPI.SessionList, '/game/<game_id>/sessions/')
         api.add_resource(DashboardAPI.Sessions, '/game/<game_id>/sessions/metrics')
         api.add_resource(DashboardAPI.Session, '/game/<game_id>/session/<session_id>/metrics')
@@ -74,6 +79,41 @@ class DashboardAPI:
                 print(f"Could not find a valid interface for {game_id}, defaulting to MySQL!")
         return ret_val
 
+    class FeatureList(Resource):
+        """Class for getting a full list of features for a given game."""
+        def get(self, game_id):
+            """Handles a GET request for a list of sessions.
+
+            :param game_id: _description_
+            :type game_id: _type_
+            :return: _description_
+            :rtype: _type_
+            """
+            print("Received metric list request.")
+            ret_val = APIResult.Default(req_type=RESTType.GET)
+
+            try:
+                feature_list = []
+                os.chdir("var/www/opengamedata/")
+                _schema = GameSchema(schema_name=f"{game_id}.json")
+                for name,percount in _schema.percount_features().items():
+                    if percount.get('enabled', False):
+                        feature_list.append(name)
+                for name,aggregate in _schema.aggregate_features().items():
+                    if aggregate.get('enabled', False):
+                        feature_list.append(name)
+                os.chdir("../../../../")
+            except Exception as err:
+                ret_val.ServerErrored(f"ERROR: Unknown error while attempting to get metric list")
+                print(f"Got exception: {str(err)}")
+                print(traceback.format_exc())
+            else:
+                if feature_list != []:
+                    ret_val.RequestSucceeded(msg="SUCCESS: Got metric list for given game", val=feature_list)
+                else:
+                    ret_val.RequestErrored("FAIL: Did not find any metrics for the given")
+            return ret_val.ToDict()
+
     class Population(Resource):
         """Class for handling requests for population-level features."""
         def get(self, game_id):
@@ -108,10 +148,13 @@ class DashboardAPI:
                     _range = ExporterRange.FromDateRange(date_min=_start_time, date_max=_end_time, source=_interface)
                     _exp_types = ExporterTypes(events=False, sessions=False, population=True)
                     _exp_locs = ExporterLocations(files=False, dict=True)
-                    request = Request(interface=_interface, range=_range, exporter_types=_exp_types, exporter_locs=_exp_locs)
+                    request = Request(interface=_interface, range=_range,
+                                      exporter_types=_exp_types, exporter_locs=_exp_locs,
+                                      feature_overrides=_metrics
+                    )
                     # retrieve and process the data
                     export_mgr = ExportManager(settings=settings)
-                    result = export_mgr.ExecuteRequest(request=request, game_id=game_id, feature_overrides=_metrics)
+                    result = export_mgr.ExecuteRequest(request=request)
                 os.chdir("../../../../")
             except Exception as err:
                 ret_val.ServerErrored(f"ERROR: Unknown error while processing data")
@@ -130,6 +173,147 @@ class DashboardAPI:
                     ret_val.RequestErrored("FAIL: No valid population features")
             return ret_val.ToDict()
 
+    class PlayerList(Resource):
+        """Class for handling requests for a list of sessions over a date range."""
+        def get(self, game_id):
+            """Handles a GET request for a list of sessions.
+
+            :param game_id: _description_
+            :type game_id: _type_
+            :return: _description_
+            :rtype: _type_
+            """
+            print("Received session list request.")
+            ret_val = APIResult.Default(req_type=RESTType.GET)
+
+            _end_time   : datetime = datetime.now()
+            _start_time : datetime = _end_time-timedelta(hours=1)
+
+            parser = reqparse.RequestParser()
+            parser.add_argument("start_datetime", type=datetime_from_iso8601, required=False, default=_start_time, nullable=True, help="Invalid starting date, defaulting to 1 hour ago.")
+            parser.add_argument("end_datetime",   type=datetime_from_iso8601, required=False, default=_end_time,   nullable=True, help="Invalid ending date, defaulting to present time.")
+            args = parser.parse_args()
+
+            _end_time   = args.get('end_datetime')   or _end_time
+            _start_time = args.get('start_datetime') or _start_time
+            try:
+                result = {}
+                os.chdir("var/www/opengamedata/")
+                _interface = DashboardAPI.gen_interface(game_id=game_id)
+                if _interface is not None:
+                    _range = ExporterRange.FromDateRange(date_min=_start_time, date_max=_end_time, source=_interface)
+                    result["ids"] = _range.GetIDs()
+                os.chdir("../../../../")
+            except Exception as err:
+                ret_val.ServerErrored(f"ERROR: Unknown error while processing data")
+                print(f"Got exception: {str(err)}")
+                print(traceback.format_exc())
+            else:
+                if result.get('ids') is not None:
+                    ret_val.RequestSucceeded(msg="SUCCESS: Got ID list for given date range", val=result['ids'])
+                else:
+                    ret_val.RequestErrored("FAIL: Did not find IDs in the given date range")
+            return ret_val.ToDict()
+
+    class Players(Resource):
+        """Class for handling requests for session-level features, given a list of session ids."""
+        def get(self, game_id):
+            """Handles a GET request for session-level features for a list of sessions.
+
+            :param game_id: _description_
+            :type game_id: _type_
+            :return: _description_
+            :rtype: _type_
+            """
+            print("Received session request.")
+            ret_val = APIResult.Default(req_type=RESTType.GET)
+
+            parser = reqparse.RequestParser()
+            parser.add_argument("player_ids")
+            parser.add_argument("metrics")
+            args = parser.parse_args()
+
+            _metrics     = DashboardAPI.parse_list(args.get('metrics') or "")
+            _player_ids = DashboardAPI.parse_list(args.get('player_ids') or "[]")
+            try:
+                result = {}
+                os.chdir("var/www/opengamedata/")
+                _interface = DashboardAPI.gen_interface(game_id=game_id)
+                if _metrics is not None and _player_ids is not None and _interface is not None:
+                    _range = ExporterRange.FromIDs(ids=_player_ids, source=_interface)
+                    _exp_types = ExporterTypes(events=False, sessions=True, population=False)
+                    _exp_locs = ExporterLocations(files=False, dict=True)
+                    request = Request(interface=_interface, range=_range,
+                                      exporter_types=_exp_types, exporter_locs=_exp_locs,
+                                      feature_overrides=_metrics
+                    )
+                    # retrieve and process the data
+                    export_mgr = ExportManager(settings=settings)
+                    result = export_mgr.ExecuteRequest(request=request)
+                os.chdir("../../../../")
+            except Exception as err:
+                ret_val.ServerErrored(f"ERROR: Unknown error while processing data")
+                print(f"Got exception: {str(err)}")
+                print(traceback.format_exc())
+            else:
+                if result.get('sessions') is not None:
+                    ret_val.RequestSucceeded(
+                        msg="SUCCESS: Generated features for given sessions",
+                        val=result['sessions']
+                    )
+                else:
+                    ret_val.RequestErrored("FAIL: No valid session features")
+            return ret_val.ToDict()
+    
+    class Player(Resource):
+        """Class for handling requests for session-level features, given a session id."""
+        def get(self, game_id, player_id):
+            """Handles a GET request for session-level features of a single Session.
+
+            :param game_id: _description_
+            :type game_id: _type_
+            :param session_id: _description_
+            :type session_id: _type_
+            :return: _description_
+            :rtype: _type_
+            """
+            ret_val = APIResult.Default(req_type=RESTType.GET)
+
+            parser = reqparse.RequestParser()
+            parser.add_argument("metrics")
+            args = parser.parse_args()
+
+            _metrics    = DashboardAPI.parse_list(args.get('metrics') or "")
+            try:
+                result = {}
+                os.chdir("var/www/opengamedata/")
+                _interface = DashboardAPI.gen_interface(game_id=game_id)
+                if _metrics is not None and _interface is not None:
+                    _range = ExporterRange.FromIDs(ids=[player_id], source=_interface)
+                    _exp_types = ExporterTypes(events=False, sessions=True, population=False)
+                    _exp_locs = ExporterLocations(files=False, dict=True)
+                    request = Request(interface=_interface, range=_range,
+                                      exporter_types=_exp_types, exporter_locs=_exp_locs,
+                                      feature_overrides=_metrics
+                    )
+                    # retrieve and process the data
+                    export_mgr = ExportManager(settings=settings)
+                    result = export_mgr.ExecuteRequest(request=request)
+                os.chdir("../../../../")
+            except Exception as err:
+                ret_val.ServerErrored(f"ERROR: Unknown error while processing data")
+                print(f"Got exception: {str(err)}")
+                print(traceback.format_exc())
+            else:
+                if result.get('sessions') is not None:
+                    ret_val.RequestSucceeded(
+                        msg="SUCCESS: Generated features for the given session",
+                        val=result['sessions']
+                    )
+                else:
+                    ret_val.RequestErrored("FAIL: No valid session features")
+            return ret_val.ToDict()
+    
     class SessionList(Resource):
         """Class for handling requests for a list of sessions over a date range."""
         def get(self, game_id):
@@ -200,10 +384,13 @@ class DashboardAPI:
                     _range = ExporterRange.FromIDs(ids=_session_ids, source=_interface)
                     _exp_types = ExporterTypes(events=False, sessions=True, population=False)
                     _exp_locs = ExporterLocations(files=False, dict=True)
-                    request = Request(interface=_interface, range=_range, exporter_types=_exp_types, exporter_locs=_exp_locs)
+                    request = Request(interface=_interface, range=_range,
+                                      exporter_types=_exp_types, exporter_locs=_exp_locs,
+                                      feature_overrides=_metrics
+                    )
                     # retrieve and process the data
                     export_mgr = ExportManager(settings=settings)
-                    result = export_mgr.ExecuteRequest(request=request, game_id=game_id, feature_overrides=_metrics)
+                    result = export_mgr.ExecuteRequest(request=request)
                 os.chdir("../../../../")
             except Exception as err:
                 ret_val.ServerErrored(f"ERROR: Unknown error while processing data")
@@ -246,10 +433,13 @@ class DashboardAPI:
                     _range = ExporterRange.FromIDs(ids=[session_id], source=_interface)
                     _exp_types = ExporterTypes(events=False, sessions=True, population=False)
                     _exp_locs = ExporterLocations(files=False, dict=True)
-                    request = Request(interface=_interface, range=_range, exporter_types=_exp_types, exporter_locs=_exp_locs)
+                    request = Request(interface=_interface, range=_range,
+                                      exporter_types=_exp_types, exporter_locs=_exp_locs,
+                                      feature_overrides=_metrics
+                    )
                     # retrieve and process the data
                     export_mgr = ExportManager(settings=settings)
-                    result = export_mgr.ExecuteRequest(request=request, game_id=game_id, feature_overrides=_metrics)
+                    result = export_mgr.ExecuteRequest(request=request)
                 os.chdir("../../../../")
             except Exception as err:
                 ret_val.ServerErrored(f"ERROR: Unknown error while processing data")
