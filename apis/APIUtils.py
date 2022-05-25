77b001f7a96b0725923ebffa59daed08285e1c4f
Luke Swanson <lwswanson2@wisc.edu>
2022-05-25 09:37:49 -0500

    Add function to get coding interface for a given game.

--- apis/APIUtils.py
+++ apis/APIUtils.py
@@ -1,14 +1,17 @@
 # import libraries
 import json
 from flask import current_app
-from typing import Any, List, Union
+from typing import Any, List, Optional, Union
 # import locals
-from config.config import settings
 import opengamedata.utils
+from config.config import settings
+from opengamedata.interfaces.CodingInterface import CodingInterface
+from opengamedata.interfaces.DataInterface import DataInterface
 from opengamedata.interfaces.MySQLInterface import MySQLInterface
 from opengamedata.interfaces.BigQueryInterface import BigQueryInterface
+from opengamedata.interfaces.BigQueryCodingInterface import BigQueryCodingInterface
 
-def parse_list(list_str:str) -> Union[List[Any], None]:
+def parse_list(list_str:str) -> Optional[List[Any]]:
     """Simple utility to parse a string containing a bracketed list into a Python list.
 
     :param list_str: _description_
@@ -23,7 +26,7 @@ def parse_list(list_str:str) -> Union[List[Any], None]:
         ret_val = list_str[start+1:end].split(",")
     return ret_val
 
-def gen_interface(game_id):
+def gen_interface(game_id) -> Optional[DataInterface]:
     """Utility to set up an Interface object for use by the API, given a game_id.
 
     :param game_id: _description_
@@ -44,4 +47,24 @@ def gen_interface(game_id):
         else:
             ret_val = MySQLInterface(game_id, settings=settings)
             current_app.logger.warning(f"Could not find a valid interface for {game_id}, defaulting to MySQL!")
+    return ret_val
+
+def gen_coding_interface(game_id) -> Optional[CodingInterface]:
+    """Utility to set up an Interface object for use by the API, given a game_id.
+
+    :param game_id: _description_
+    :type game_id: _type_
+    :return: _description_
+    :rtype: _type_
+    """
+    ret_val = None
+    src_map = settings['GAME_SOURCE_MAP'].get(game_id)
+    if src_map is not None:
+        # set up interface and request
+        if src_map['interface'] == "BigQuery":
+            ret_val = BigQueryCodingInterface(game_id=game_id, settings=settings)
+            current_app.logger.info(f"Using BigQueryCodingInterface for {game_id}")
+        else:
+            ret_val = BigQueryCodingInterface(game_id=game_id, settings=settings)
+            current_app.logger.warning(f"Could not find a valid interface for {game_id}, defaulting to BigQuery!")
     return ret_val
