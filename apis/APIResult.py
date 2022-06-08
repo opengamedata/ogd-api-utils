f29d2e05a9fb32bb26c3798eccaeb43ca351e68c
Luke Swanson <lwswanson2@wisc.edu>
2022-06-08 09:16:37 -0500

    Better organization, and remove most of the setters, better to go through more intelligent functions.

--- apis/APIResult.py
+++ apis/APIResult.py
@@ -69,6 +69,54 @@ class APIResult:
             status=ResultStatus.NONE
         )
 
+    @staticmethod
+    def FromRequestResult(result:RequestResult.RequestResult, req_type:RESTType):
+        _status : ResultStatus
+        if result.Status == RequestResult.ResultStatus.SUCCESS:
+            _status = ResultStatus.SUCCESS 
+        elif result.Status == RequestResult.ResultStatus.FAILURE:
+            _status = ResultStatus.ERR_REQ
+        else:
+            _status = ResultStatus.ERR_SRV
+        ret_val = APIResult(req_type=req_type, val=result.ValuesDict, msg=result.Message, status=_status)
+        return ret_val
+
+    @property
+    def Type(self) -> RESTType:
+        """Property for the type of REST request
+
+        :return: A RESTType representing the type of REST request
+        :rtype: _type_
+        """
+        return self._type
+
+    @property
+    def Value(self) -> Any:
+        """Property for the value of the request result.
+
+        :return: Some value, of any type, returned from the request.
+        :rtype: Any
+        """
+        return self._val
+
+    @property
+    def Message(self) -> str:
+        """Property for the message associated with a request result.
+
+        :return: A string message giving details on the result of the request.
+        :rtype: str
+        """
+        return self._msg
+
+    @property
+    def Status(self) -> ResultStatus:
+        """Property for the status of the request.
+
+        :return: A ResultStatus indicating whether request is/was successful, incomplete, failed, etc.
+        :rtype: ResultStatus
+        """
+        return self._status
+
     def RequestErrored(self, msg:str):
         self._status = ResultStatus.ERR_REQ
         self._msg = msg
@@ -82,18 +130,6 @@ class APIResult:
         self._msg = msg
         self._val = val
 
-    @staticmethod
-    def FromRequestResult(result:RequestResult.RequestResult, req_type:RESTType):
-        _status : ResultStatus
-        if result.Status == RequestResult.ResultStatus.SUCCESS:
-            _status = ResultStatus.SUCCESS 
-        elif result.Status == RequestResult.ResultStatus.FAILURE:
-            _status = ResultStatus.ERR_REQ
-        else:
-            _status = ResultStatus.ERR_SRV
-        ret_val = APIResult(req_type=req_type, val=result.ValuesDict, msg=result.Message, status=_status)
-        return ret_val
-
     def ToDict(self):
         return {
             "type"   : str(self._type),
@@ -101,51 +137,3 @@ class APIResult:
             "msg"    : self._msg,
             "status" : str(self._status)
         }
-
-    @property
-    def Type(self) -> RESTType:
-        """Property for the type of REST request
-
-        :return: A RESTType representing the type of REST request
-        :rtype: _type_
-        """
-        return self._type
-    @Type.setter
-    def Type(self, new_type:RESTType):
-        self._type = new_type
-
-    @property
-    def Value(self) -> Any:
-        """Property for the value of the request result.
-
-        :return: Some value, of any type, returned from the request.
-        :rtype: Any
-        """
-        return self._val
-    @Value.setter
-    def Value(self, new_val:Any):
-        self._val = new_val
-
-    @property
-    def Message(self) -> str:
-        """Property for the message associated with a request result.
-
-        :return: A string message giving details on the result of the request.
-        :rtype: str
-        """
-        return self._msg
-    @Message.setter
-    def Message(self, new_msg:str):
-        self._msg = new_msg
-
-    @property
-    def Status(self) -> ResultStatus:
-        """Property for the status of the request.
-
-        :return: A ResultStatus indicating whether request is/was successful, incomplete, failed, etc.
-        :rtype: ResultStatus
-        """
-        return self._status
-    @Status.setter
-    def Status(self, new_status:ResultStatus):
-        self._status = new_status
