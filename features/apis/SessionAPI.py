1ac5965feec39f59c97f38d42aecb9917bd4fe84
Luke Swanson <lwswanson2@wisc.edu>
2023-11-08 13:40:36 -0600

    Update SessionAPI to work with newer ogd-core functions.

--- features/apis/SessionAPI.py
+++ features/apis/SessionAPI.py
@@ -12,13 +12,16 @@ from typing import Optional, Union
 # import locals
 from utils.APIResult import APIResult, RESTType, ResultStatus
 from utils import APIUtils
-from config.config import settings
+from config.config import settings as server_settings
+from opengamedata.config.config import settings as core_settings
 from opengamedata.interfaces.DataInterface import DataInterface
 from opengamedata.interfaces.outerfaces.DictionaryOuterface import DictionaryOuterface
 from opengamedata.managers.ExportManager import ExportManager
 from opengamedata.ogd_requests.Request import Request, ExporterRange, IDMode
 from opengamedata.ogd_requests.RequestResult import RequestResult
 from opengamedata.schemas.ExportMode import ExportMode
+from opengamedata.schemas.configs.ConfigSchema import ConfigSchema
+from opengamedata.schemas.configs.GameSourceSchema import GameSourceSchema
 from opengamedata.schemas.games.GameSchema import GameSchema
 
 class SessionAPI:
@@ -63,9 +66,8 @@ class SessionAPI:
             _start_time = args.get('start_datetime') or _start_time
             _range : Union[ExporterRange, None] = None
             try:
-                
                 orig_cwd = os.getcwd()
-                os.chdir(settings["OGD_CORE_PATH"])
+                os.chdir(server_settings["OGD_CORE_PATH"])
 
                 _interface : Union[DataInterface, None] = APIUtils.gen_interface(game_id=game_id)
                 if _interface is not None:
@@ -110,19 +112,20 @@ class SessionAPI:
                 values_dict = {}
                 
                 orig_cwd = os.getcwd()
-                os.chdir(settings["OGD_CORE_PATH"])
+                os.chdir(server_settings["OGD_CORE_PATH"])
+                _core_config = ConfigSchema(name="Core Config", all_elements=core_settings)
 
                 _interface : Union[DataInterface, None] = APIUtils.gen_interface(game_id=game_id)
                 if _metrics is not None and _session_ids is not None and _interface is not None:
                     _range = ExporterRange.FromIDs(source=_interface, ids=_session_ids, id_mode=IDMode.SESSION)
-                    _exp_types = set([ExportMode.SESSION])
-                    _outerface = DictionaryOuterface(game_id=game_id, out_dict=values_dict)
+                    _exp_types = {ExportMode.SESSION}
+                    _outerface = DictionaryOuterface(game_id=game_id, config=GameSourceSchema.EmptySchema(), export_modes=_exp_types, out_dict=values_dict)
                     request    = Request(interface=_interface,      range=_range,
                                          exporter_modes=_exp_types, outerfaces={_outerface},
                                          feature_overrides=_metrics
                     )
                     # retrieve and process the data
-                    export_mgr = ExportManager(settings=settings)
+                    export_mgr = ExportManager(config=_core_config)
                     result = export_mgr.ExecuteRequest(request=request)
                 elif _metrics is None:
                     current_app.logger.warning("_metrics was None")
@@ -176,19 +179,20 @@ class SessionAPI:
                 values_dict = {}
                 
                 orig_cwd = os.getcwd()
-                os.chdir(settings["OGD_CORE_PATH"])
+                os.chdir(server_settings["OGD_CORE_PATH"])
+                _core_config = ConfigSchema(name="Core Config", all_elements=core_settings)
 
                 _interface : Optional[DataInterface] = APIUtils.gen_interface(game_id=game_id)
                 if _metrics is not None and _interface is not None:
                     _range = ExporterRange.FromIDs(source=_interface, ids=[session_id], id_mode=IDMode.SESSION)
-                    _exp_types = set([ExportMode.SESSION])
-                    _outerface = DictionaryOuterface(game_id=game_id, out_dict=values_dict)
+                    _exp_types = {ExportMode.SESSION}
+                    _outerface = DictionaryOuterface(game_id=game_id, config=GameSourceSchema.EmptySchema(), export_modes=_exp_types, out_dict=values_dict)
                     request    = Request(interface=_interface,      range=_range,
                                          exporter_modes=_exp_types, outerfaces={_outerface},
                                          feature_overrides=_metrics
                     )
                     # retrieve and process the data
-                    export_mgr = ExportManager(settings=settings)
+                    export_mgr = ExportManager(config=_core_config)
                     result = export_mgr.ExecuteRequest(request=request)
                 elif _metrics is None:
                     current_app.logger.warning("_metrics was None")
@@ -242,14 +246,14 @@ class SessionAPI:
                 feature_list = []
                 
                 orig_cwd = os.getcwd()
-                os.chdir(settings["OGD_CORE_PATH"])
+                os.chdir(server_settings["OGD_CORE_PATH"])
 
                 _schema = GameSchema(schema_name=f"{game_id}.json")
                 for name,percount in _schema.PerCountFeatures.items():
-                    if percount.get('enabled', False):
+                    if ExportMode.SESSION in percount.Enabled:
                         feature_list.append(name)
                 for name,aggregate in _schema.AggregateFeatures.items():
-                    if aggregate.get('enabled', False):
+                    if ExportMode.SESSION in aggregate.Enabled:
                         feature_list.append(name)
                 os.chdir(orig_cwd)
             except Exception as err:
