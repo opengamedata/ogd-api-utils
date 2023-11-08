301d41561ee50217953772135498ead5381664c5
Luke Swanson <lwswanson2@wisc.edu>
2023-11-08 13:06:05 -0600

    Update APIUtils to use newer params from ogd core.

--- utils/APIUtils.py
+++ utils/APIUtils.py
@@ -2,12 +2,15 @@
 from flask import current_app
 from typing import Any, Dict, List, Optional
 # import locals
-from config.config import settings
+from config.config import settings as server_settings
+from opengamedata.config.config import settings as core_settings
 from opengamedata.interfaces.CodingInterface import CodingInterface
 from opengamedata.interfaces.DataInterface import DataInterface
 from opengamedata.interfaces.MySQLInterface import MySQLInterface
 from opengamedata.interfaces.BigQueryInterface import BigQueryInterface
 from opengamedata.interfaces.BigQueryCodingInterface import BigQueryCodingInterface
+from opengamedata.schemas.configs.ConfigSchema import ConfigSchema
+from opengamedata.schemas.configs.GameSourceSchema import GameSourceSchema
 
 def parse_list(list_str:str) -> Optional[List[Any]]:
     """Simple utility to parse a string containing a bracketed list into a Python list.
@@ -33,21 +36,20 @@ def gen_interface(game_id) -> Optional[DataInterface]:
     :rtype: _type_
     """
     ret_val = None
-    _source_name = settings['GAME_SOURCE_MAP'][game_id]['source']
-    _source : Dict[str, Any] = settings['GAME_SOURCES'][_source_name]
+    
+    _core_config = ConfigSchema(name="Core Config", all_elements=core_settings)
+    _game_source : GameSourceSchema = _core_config.GameSourceMap.get(game_id, GameSourceSchema.EmptySchema())
 
-    config = settings.get('GAME_SOURCE_MAP', {}).get(game_id)
-    if config is not None:
-        config['source'] = {key:val for key, val in _source.items()}
+    if _game_source.Source is not None:
         # set up interface and request
-        if config['source']['DB_TYPE'] == "MySQL":
-            ret_val = MySQLInterface(game_id, config=config)
+        if _game_source.Source.Type == "MySQL":
+            ret_val = MySQLInterface(game_id, config=_game_source, fail_fast=False)
             current_app.logger.info(f"Using MySQLInterface for {game_id}")
-        elif config['source']['DB_TYPE'] == "BigQuery":
-            ret_val = BigQueryInterface(game_id=game_id, config=config)
+        elif _game_source.Source.Type == "BigQuery":
+            ret_val = BigQueryInterface(game_id=game_id, config=_game_source, fail_fast=False)
             current_app.logger.info(f"Using BigQueryInterface for {game_id}")
         else:
-            ret_val = MySQLInterface(game_id, config=config)
+            ret_val = MySQLInterface(game_id, config=_game_source, fail_fast=False)
             current_app.logger.warning(f"Could not find a valid interface for {game_id}, defaulting to MySQL!")
     return ret_val
 
@@ -60,13 +62,16 @@ def gen_coding_interface(game_id) -> Optional[CodingInterface]:
     :rtype: _type_
     """
     ret_val = None
-    src_map = settings['GAME_SOURCE_MAP'].get(game_id)
-    if src_map is not None:
+
+    _core_config = ConfigSchema(name="Core Config", all_elements=core_settings)
+    _game_source : GameSourceSchema = _core_config.GameSourceMap.get(game_id, GameSourceSchema.EmptySchema())
+
+    if _game_source.Source is not None:
         # set up interface and request
-        if src_map['DB_TYPE'] == "BigQuery":
-            ret_val = BigQueryCodingInterface(game_id=game_id, settings=settings)
+        if _game_source.Source.Type == "BigQuery":
+            ret_val = BigQueryCodingInterface(game_id=game_id, config=_core_config)
             current_app.logger.info(f"Using BigQueryCodingInterface for {game_id}")
         else:
-            ret_val = BigQueryCodingInterface(game_id=game_id, settings=settings)
+            ret_val = BigQueryCodingInterface(game_id=game_id, config=_core_config)
             current_app.logger.warning(f"Could not find a valid interface for {game_id}, defaulting to BigQuery!")
     return ret_val
