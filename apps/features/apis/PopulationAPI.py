fb8091f88f534a2a2454303100e62b34b0f24d7b
Luke Swanson <lwswanson2@wisc.edu>
2024-01-25 11:14:21 -0600

    Ok, try JSON-ifying before putting into the response object.

--- apps/features/apis/PopulationAPI.py
+++ apps/features/apis/PopulationAPI.py
@@ -1,7 +1,7 @@
 """Module for the Population API code
 """
 # import libraries
-import os
+import json
 import traceback
 from datetime import datetime, timedelta
 from pathlib import Path
@@ -121,7 +121,7 @@ class PopulationAPI:
                 else:
                     api_result.RequestErrored("No valid population features")
             finally:
-                return Response(response=api_result.ToDict(), status=api_result.Status.value, mimetype='application/json')
+                return Response(response=json.dumps(api_result.ToDict()), status=api_result.Status.value, mimetype='application/json')
                 # return api_result.ToDict()
 
     class PopulationFeatureList(Resource):
