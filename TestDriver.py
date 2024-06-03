6103d627b1579d8e7a3e611efe06a166c16a86fa
Luke Swanson <lwswanson2@wisc.edu>
2024-06-03 11:05:05 -0500

    Add config item for testing the config schema.

--- TestDriver.py
+++ TestDriver.py
@@ -1,6 +1,7 @@
-from tests.cases.utils.t_HelloAPI import t_HelloAPI
 from tests.cases.utils.t_APIResponse import t_APIResponse
 from tests.cases.utils.t_APIUtils import t_APIUtils
+from tests.cases.schemas.t_ServerConfigSchema import t_ServerConfigSchema
+from tests.cases.utils.t_HelloAPI import t_HelloAPI
 
 from ogd.core.schemas.configs.TestConfigSchema import TestConfigSchema
 from tests.config.t_config import settings
@@ -8,10 +9,10 @@ from tests.config.t_config import settings
 _config = TestConfigSchema.FromDict(name="APIUtilsTestConfig", all_elements=settings, logger=None)
 
 
-if _config.EnabledTests.get('HELLO'):
-    test_Hello = t_HelloAPI()
-    print("***\nRunning test_Hello:")
-    test_Hello.RunAll()
+if _config.EnabledTests.get('CONFIG'):
+    test_Utils = t_ServerConfigSchema()
+    print("***\nRunning test_ServerConfigSchema:\n")
+    test_Utils.RunAll()
     print("Done\n***")
 if _config.EnabledTests.get('RESPONSE'):
     test_APIResponse = t_APIResponse()
@@ -20,6 +21,11 @@ if _config.EnabledTests.get('RESPONSE'):
     print("Done\n***")
 if _config.EnabledTests.get('UTILS'):
     test_Utils = t_APIUtils()
-    print("***\nRunning test_Dashboard:\n")
+    print("***\nRunning test_APIUtils:\n")
     test_Utils.RunAll()
+    print("Done\n***")
+if _config.EnabledTests.get('HELLO'):
+    test_Hello = t_HelloAPI()
+    print("***\nRunning test_Hello:")
+    test_Hello.RunAll()
     print("Done\n***")
