13bcc11103962c38b7dcbec6521330cfd5738f0d
Luke Swanson <lwswanson2@wisc.edu>
2024-06-03 11:40:03 -0500

    Use core logger class for logging in t_APIUtils.

--- tests/cases/utils/t_APIUtils.py
+++ tests/cases/utils/t_APIUtils.py
@@ -1,8 +1,10 @@
 # import libraries
+import logging
 import unittest
 from unittest import TestCase
 # import ogd libraries.
 from ogd.core.schemas.configs.TestConfigSchema import TestConfigSchema
+from ogd.core.utils.Logger import Logger
 # import locals
 from src.ogd.apis.utils.APIUtils import parse_list, gen_interface
 from tests.config.t_config import settings
@@ -15,24 +17,28 @@ class t_APIUtils:
         pass
 
 class t_ParseList(TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        Logger.InitializeLogger(level=logging.INFO, use_logfile=False)
+
     def test_parse_list_empty(self):
         list_str = "[]"
-        parsed = parse_list(list_str=list_str)
+        parsed = parse_list(list_str=list_str, logger=Logger.std_logger)
         self.assertEqual(parsed, [])
 
     def test_parse_list_one_elem(self):
         list_str = "[FeatureName]"
-        parsed = parse_list(list_str=list_str)
+        parsed = parse_list(list_str=list_str, logger=Logger.std_logger)
         self.assertEqual(parsed, ["FeatureName"])
 
     def test_parse_list_three_elem(self):
         list_str = "[FeatureName1, FeatureName2, FeatureName3]"
-        parsed = parse_list(list_str=list_str)
+        parsed = parse_list(list_str=list_str, logger=Logger.std_logger)
         self.assertEqual(parsed, ["FeatureName1, FeatureName2, FeatureName3"])
 
     def test_parse_list_invalid(self):
         list_str = "Not a [list] type of thing"
-        parsed = parse_list(list_str=list_str)
+        parsed = parse_list(list_str=list_str, logger=Logger.std_logger)
         self.assertEqual(parsed, None)
 
 @unittest.skip("No tests written yet")
