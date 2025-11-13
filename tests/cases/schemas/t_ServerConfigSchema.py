f89954203e54130bfd310cee0e5b777d31a9edcb
Luke Swanson <lwswanson2@wisc.edu>
2025-11-13 15:20:50 -0600

    Update ServerConfig testbed.

--- tests/cases/schemas/t_ServerConfigSchema.py
+++ tests/cases/schemas/t_ServerConfigSchema.py
@@ -1,27 +1,46 @@
 # import libraries
+import logging
 import unittest
 from unittest import TestCase
 # import ogd libraries.
-from ogd.common.schemas.configs.TestConfigSchema import TestConfigSchema
+from ogd.common.configs.TestConfig import TestConfig
+from ogd.common.models.SemanticVersion import SemanticVersion
+from ogd.common.utils.Logger import Logger
 # import locals
 from src.ogd.apis.configs.ServerConfig import ServerConfig
 from tests.config.t_config import settings
 
-_config = TestConfigSchema.FromDict(name="ServerConfigSchemaTestConfig", all_elements=settings, logger=None)
+_config = TestConfig.FromDict(name="ServerConfigSchemaTestConfig", unparsed_elements=settings)
 
-@unittest.skip("No tests implemented yet")
 class t_ServerConfigSchema(TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        # 1. Get testing config
+        _testing_cfg = TestConfig.FromDict(name="SchemaTestConfig", unparsed_elements=settings)
+        _level     = logging.DEBUG if _testing_cfg.Verbose else logging.INFO
+        Logger.std_logger.setLevel(_level)
+
+        # 2. Set up local instance of class to test
+        cls.test_schema = ServerConfig(
+            name="Server Configuration",
+            debug_level=logging.WARN,
+            version=SemanticVersion(major=1, minor=2, patch=3, suffix="alpha"),
+            other_elements={ "foo":"bar" }
+        )
+
     @staticmethod
     def RunAll():
         pass
 
-    @unittest.skip("Not yet implemented")
     def test_DebugLevel(self):
-        pass
+        _lvl = self.test_schema.DebugLevel
+        self.assertIsInstance(_lvl, int)
+        self.assertEqual(_lvl, logging.WARN)
 
-    @unittest.skip("Not yet implemented")
     def test_Version(self):
-        pass
+        _ver = self.test_schema.Version
+        self.assertIsInstance(_ver, SemanticVersion)
+        self.assertEqual(_ver, SemanticVersion(1, 2, 3, "alpha"))
 
     @unittest.skip("Not yet implemented")
     def test_AsMarkdown(self):
