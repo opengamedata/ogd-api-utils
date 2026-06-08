f8a2a0e6b8d33440b78fb153ff1a33696c7f0acd
Luke Swanson <lwswanson2@wisc.edu>
2026-06-07 21:38:30 -0500

    Add a test case for urljoin function.

---
+++ tests/cases/utils/APIUtilsSuite/URLJoinCase.py
@@ -0,0 +1,44 @@
+# import libraries
+import unittest
+from unittest import TestCase
+# import ogd libraries.
+from ogd.common.utils.Logger import Logger
+# import locals
+try:
+    from src.ogd.apis.utils.APIUtils import urljoin
+except ModuleNotFoundError as err:
+    Logger.Log(f"Import error: {err}")
+finally:
+    from tests.config.t_config import settings
+
+class URLJoinCase(TestCase):
+    """Test case for the `urljoin` utility function.
+
+    This is one of those weird cases where we've actually got standalone functions.
+    They each get a 'case' for calling in a number of different ways.
+
+    :param TestCase: _description_
+    :type TestCase: _type_
+    """
+    def test_noslashes_noscheme(self):
+        base = "ogd-services.fielddaylab.wisc.edu"
+        loc  = "app.wsgi/path/to/endpoint"
+        self.assertEqual(urljoin(base=base, url=loc), f"{base}/{loc}")
+
+    def test_baseslash_noscheme(self):
+        base = "ogd-services.fielddaylab.wisc.edu/"
+        loc  = "app.wsgi/path/to/endpoint"
+        self.assertEqual(urljoin(base=base, url=loc), f"{base[:-1]}/{loc}")
+
+    def test_urlslash_noscheme(self):
+        base = "ogd-services.fielddaylab.wisc.edu"
+        loc  = "/app.wsgi/path/to/endpoint"
+        self.assertEqual(urljoin(base=base, url=loc), f"{base}/{loc[1:]}")
+
+    def test_bothslash_noscheme(self):
+        base = "ogd-services.fielddaylab.wisc.edu/"
+        loc  = "/app.wsgi/path/to/endpoint"
+        self.assertEqual(urljoin(base=base, url=loc), f"{base[:-1]}/{loc[1:]}")
+
+if __name__ == '__main__':
+    unittest.main()
