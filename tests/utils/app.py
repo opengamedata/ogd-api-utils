f4afe8701bde1bb5d85aa8152b00dfa6cf517796
Luke Swanson <lwswanson2@wisc.edu>
2026-09-08 20:09:50 -0500

    Import from ogd.apis, not directly apis, because app.py is going to be outside of ogd folder.

--- tests/utils/app.py
+++ tests/utils/app.py
@@ -28,8 +28,8 @@ sys.path.insert(0, sys.path.pop()) # Move venv sitedir to front of sys.path
 
 # 3. Register api
 try:
-    from apis.configs.ServerConfig import ServerConfig
-    from apis.HelloAPI import HelloAPI
+    from ogd.apis.configs.ServerConfig import ServerConfig
+    from ogd.apis.HelloAPI import HelloAPI
 except ImportError as err:
     _logImportErr(msg="Could not import Hello API, an ImportError occurred:", err=err)
 except Exception as err:
